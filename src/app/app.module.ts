45cd6d3bc4ccd1084542760c070269451d72195d
Shanmuga Ganesh <ksganesh.shanmu@gmail.com>
2017-03-22 07:56:27 +0530

    IonicFire

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { AngularFireModule } from 'angularfire2';
+
+export const firebaseConfig = {
+ 
+    apiKey: "AIzaSyDLTLdEiHAYYG4uFIROPLFcAodxTs34psk",
+    authDomain: "ionicfireone.firebaseapp.com",
+    databaseURL: "https://ionicfireone.firebaseio.com",
+    storageBucket: "ionicfireone.appspot.com",
+    messagingSenderId: "925185985399"
+
+};
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp),
+     AngularFireModule.initializeApp(firebaseConfig)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
